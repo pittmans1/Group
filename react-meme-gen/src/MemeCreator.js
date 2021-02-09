cbc1e91871541bd2356736e5020a9d4f76d2be4a
Triston Frischkencht <74280137+TristonF97@users.noreply.github.com>
2021-02-09 13:50:07 -0700

    Updated form and added text preview

--- react-meme-gen/src/MemeCreator.js
+++ react-meme-gen/src/MemeCreator.js
@@ -4,21 +4,58 @@ class MemeCreator extends React.Component {
     constructor() {
         super()
         this.state= {
-
+            topLine: "",
+            bottomLine: ""
         }
+        this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
-    }
-
-    handleSubmit() {
         
     }
 
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit() {
+      
+    }
+
     render() {
+
         return (
             <div>
-                <input type="text" placeholder="Top Text"/>
-                <input type="text" placeholder="Bottom Text"/>
-                <button onSubmit={this.handleSubmit}>Submit Meme</button>
+
+                <form>
+                    <input
+                    type="text"
+                    name="topLine"
+                    onChange={this.handleChange}
+                    placeholder="Top Text"
+                    />
+
+                    <br />
+                    
+                    <input
+                        type="text"
+                        name="bottomLine"
+                        onChange={this.handleChange}
+                        placeholder="Bottom Text"
+                    />
+
+                    <br />
+
+                    <button
+                        onSubmit={this.handleSubmit}>
+                        Submit Meme
+                    </button>
+                </form>
+
+                <h1> {this.state.topLine} </h1>
+
+                <h1> {this.state.bottomLine} </h1>
+
             </div>
         )
     }
