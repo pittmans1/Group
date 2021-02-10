694b559e7457fea3993f45ea7b2cf3add6808892
Triston Frischkencht <74280137+TristonF97@users.noreply.github.com>
2021-02-10 09:55:38 -0700

    Updated Header.js

--- react-meme-gen/src/Header.js
+++ react-meme-gen/src/Header.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function Header(){
     return(
-        <header class="header">
+        <header className="header">
             <img style={{paddingLeft:25, paddingRight:25}} src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="meme" height="50px" width="50px" />
             <span style={{fontSize: "70px"}}> Meme Generator </span>
         </header>
