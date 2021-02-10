21759a5868f08111e835954c9c0456a6a7df09a0
Triston Frischkencht <74280137+TristonF97@users.noreply.github.com>
2021-02-10 09:55:07 -0700

    Updated App.js

--- react-meme-gen/src/App.js
+++ react-meme-gen/src/App.js
@@ -3,39 +3,14 @@ import style from "./style.css"
 import MemeCreator from "./MemeCreator"
 import Header from "./Header"
 
-class App extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            meme: []
-        }
-        this.handleClick = this.handleClick.bind(this)
-    }
-    handleClick(){
-       
-            fetch("https://api.imgflip.com/get_memes")
-            .then(response => response.json())
-             .then(data => {
-               this.setState({
-                meme: data
-                })
-            })
-        
-    }
-    
-    render(){
-        return(
-            <div>
-                <Header />
-                <div>{this.state.meme.map(item => <h1 >{item.name}</h1>)}</div>
-                <div>{this.state.meme.map(img => <img src={img.url} style={{width: 1200, height: 1200}}/>)}</div>
-                <button onClick= {this.handleClick} name="button">Click for Meme</button>
+function App() {
+    return(
+        <div>
+            <Header />
             
-                <MemeCreator />
-            </div>
-        )
-    }
-    
+            <MemeCreator />
+        </div>
+    )
 }
 
 export default App
