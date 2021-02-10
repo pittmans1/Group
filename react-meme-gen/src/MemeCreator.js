4b01be46c0a7fd716d20f19c5be34c4022f2372d
Triston Frischkencht <74280137+TristonF97@users.noreply.github.com>
2021-02-10 09:56:45 -0700

    Updated MemeCreator.js

--- react-meme-gen/src/MemeCreator.js
+++ react-meme-gen/src/MemeCreator.js
@@ -1,15 +1,35 @@
 import React from "react"
+import Meme from "./Meme.js"
+import MemeForm from "./MemeForm.js"
 
 class MemeCreator extends React.Component {
     constructor() {
         super()
         this.state= {
             topLine: "",
-            bottomLine: ""
+            imgUrl: "",
+            bottomLine: "",
+            memeList: []
         }
+        this.handleClick = this.handleClick.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
-        
+    }
+
+    /*Think we need a componentDidMount here with another fetch
+    to load a meme image when the site is opened*/
+
+    handleClick(){
+        fetch("https://api.imgflip.com/get_memes")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            this.setState ({
+                topLine: "",
+                imgUrl: data.memes,
+                bottomLine:""
+            })
+        })
     }
 
     handleChange(event) {
@@ -18,44 +38,30 @@ class MemeCreator extends React.Component {
         })
     }
 
-    handleSubmit() {
-      
+    handleSubmit(event) {
+      event.preventDefault()
+      const newMeme = [...this.state.memeList]
+
+      newMeme.push(this.state)
+      this.setState({
+          memeList: newMeme
+      })
     }
 
     render() {
-
+        let memes = this.state.memeList.map(meme => <Meme info={meme}/>)
         return (
             <div>
+                <MemeForm
+                    state={this.state}
+                    handleClick={this.handleClick}
+                    handleChange={this.handleChange}
+                    handleSubmit={this.handleSubmit}
+                />
 
-                <form>
-                    <input
-                    type="text"
-                    name="topLine"
-                    onChange={this.handleChange}
-                    placeholder="Top Text"
-                    />
+                <button onClick= {this.handleClick} name="button">Click for Meme</button>
 
-                    <br />
-                    
-                    <input
-                        type="text"
-                        name="bottomLine"
-                        onChange={this.handleChange}
-                        placeholder="Bottom Text"
-                    />
-
-                    <br />
-
-                    <button
-                        onSubmit={this.handleSubmit}>
-                        Submit Meme
-                    </button>
-                </form>
-
-                <h1> {this.state.topLine} </h1>
-
-                <h1> {this.state.bottomLine} </h1>
-
+                {memes}
             </div>
         )
     }
