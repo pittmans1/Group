6243bffda1f5085710498c9435e5ffc3a6d88fc4
scott pittman <sp.digital97@gmail.com>
2021-02-10 11:32:09 -0500

    started mapping

--- react-meme-gen/src/App.js
+++ react-meme-gen/src/App.js
@@ -7,26 +7,28 @@ class App extends React.Component{
     constructor(){
         super()
         this.state = {
-            name: "",
-            url: "",
-            id:""
+            meme: []
         }
         this.handleClick = this.handleClick.bind(this)
     }
     handleClick(){
-        fetch("https://api.imgflip.com/get_memes")
-        .then(response => response.json())
-        .then(data => {
-            this.state.map()
-        })
+       
+            fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+             .then(data => {
+               this.setState({
+                meme: data
+                })
+            })
+        
     }
     
     render(){
         return(
             <div>
                 <Header />
-                <h1> {this.state.name} </h1>
-                <img src={this.state.url} style={{width: 1200, height: 1200}}/>
+                <div>{this.state.meme.map(item => <h1 >{item.name}</h1>)}</div>
+                <div>{this.state.meme.map(img => <img src={img.url} style={{width: 1200, height: 1200}}/>)}</div>
                 <button onClick= {this.handleClick} name="button">Click for Meme</button>
             
                 <MemeCreator />
