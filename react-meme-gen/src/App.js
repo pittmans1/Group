fbdd92abe9bd8841aa24773f736cdf25bdbe3f24
Triston Frischkencht <74280137+TristonF97@users.noreply.github.com>
2021-02-11 09:29:50 -0700

    Everything but image on page load and edit/delete button functionality

--- react-meme-gen/src/App.js
+++ react-meme-gen/src/App.js
@@ -2,16 +2,11 @@ import React from "react"
 import style from "./style.css"
 import MemeCreator from "./MemeCreator"
 import Header from "./Header"
-import Meme from "./Meme"
-
-
 
 function App() {
     return(
         <div>
             <Header />
-            <Meme />
-            
             <MemeCreator />
         </div>
     )
